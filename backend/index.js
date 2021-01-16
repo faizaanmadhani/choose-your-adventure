11b90b852749b8dbeb46de7eed5fac2c00d65e4d
Luna Liu <lunal22@yorkhouse.ca>
2021-01-16 13:00:02 -0800

    switching branches

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const SERVER_STARTED = 'Server started on port: '
-const PAGES_ARRAY = []; 
+const db = admin.firestore();
 
 /* app.get('/', (req, res) => {
     res.send("Hello, World");
@@ -10,18 +10,16 @@ const PAGES_ARRAY = [];
 
 app.listen(port, () => {
     console.log(SERVER_STARTED + port);
-});
+}); 
 
-function removePage(page){
-    var index = PAGES_ARRAY.indexOf(page);
-    if(index > -1){
-        PAGES_ARRAY.splice(index, 1); //removing it from list of pages
-    } 
-
-    var i = 0;
-    while (i < page.children.length){
-       removeChildren(page, page.children[i]); //removing it from connected pages
-    }
+async function removePage(db){
+    const admin = require('firebase-admin');
+    const FieldValue = admin.firestore.FieldValue;
+    const ref = db.collection('stories').doc('');
+    const res = await ref.update({
+        capital: FieldValue.delete()
+    });
+    console.log('Update: ', res);
 }
 
 function removeChildren(page1, page2){ 
@@ -32,24 +30,15 @@ function removeChildren(page1, page2){
     }
 }
 
-function addPage(){
-    Page newPage = new Page;
-    PAGES_ARRAY.push(newPage);
-    console.log(PAGES_ARRAY);
-}
+async function addPage(db){
+    const ref = await db.collection('stories').add({
+        name: '',
+        choice: ''
+    }); 
+    console.log('Add: ', res);
+} 
 
-class Page{
-    constructor(text, children){
-        this.text = new String("Text");
-        this.children = new Array();
-    }
-
-    setText(choice){
-        this.text = choice;
-    } 
-
-    addChildren(choice){
-        this.children.push(choice);
-        choice.children.push(this.Page);
-    }
+function connectPages(page1, page2){
+    page1.addChildren(page2);
+    page2.addChildren(page1);
 }
