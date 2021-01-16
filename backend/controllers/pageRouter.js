27a4e533517ac1835d4b755cef57b9edad5669fa
Faizaan Madhani <fzmadhani@gmail.com>
2021-01-16 15:58:52 -0500

    Add pull and create route

--- backend/controllers/pageRouter.js
+++ backend/controllers/pageRouter.js
@@ -28,21 +28,46 @@ pageRouter.get('/', async (req, res) => {
     const documentRef = db.collection('data').doc('stories')
     documentRef.listCollections().then(collections => {
         for (let collection of collections) {
-          collectionIDs.push({"id" : collection.id, "name": collection.doc('config').title})
+          collectionIDs.push({
+            "id" : collection.id, 
+            "info": collection.document('config').data()
+            })
         }
         res.status(200).send({stories: collectionIDs})
       });
 
 });
 
-// Create a new storys
+// Create a new story
 pageRouter.post('/create', async (req, res) => {
-    storiesReference.collection(req.name).doc('config').set({
-        title: req.title,
-        author: req.author,
-        zoom: req.zoom,
-        position: [position.x, position.y]
-    })
+
+    // Create collection doc
+    try {
+        const docRef = await storiesReference.collection(req.name).doc('config').set({
+            title: req.title,
+            author: req.author,
+            zoom: req.zoom,
+            position: {"x" : req.pos.x, "y" : pos.y}
+        })
+    } catch (e) {
+        res.status(500).send({"msg": e})
+    }
+
+    res.status(200).send({"msg": "New story created"})
+
+})
+
+pageRouter.get('/:title', async (req, res) => {
+    const story_title = req.params["title"]
+    try {
+        const storyPages = await storiesReference.collection(story_title).get()
+        const pagesMap = storyPages.docs.map((doc) => doc.data());
+        const configPage = pagesMap.filter(doc => doc.id == 'config')
+        pagesMap = pagesMap.filter(doc => doc.id !== 'config')
+        res.status(200).send({"story" : pagesMap})
+    } catch (e) {
+        res.status(500).sendStatus({"msg": e})
+    }
 })
 
 module.exports = pageRouter
