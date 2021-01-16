ce9cd27a19710b3f58417565d8f9d40f2cfbe18c
Faizaan Madhani <fzmadhani@gmail.com>
2021-01-16 01:17:51 -0500

    Backend boilerplate

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const SERVER_STARTED = 'Server started on port: '
+
+app.get('/', (req, res) => {
+    res.send("Hello, World");
+});
+
+app.listen(port, () => {
+    console.log(SERVER_STARTED + port);
+});
+
+
