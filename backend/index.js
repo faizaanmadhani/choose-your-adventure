f3418f380a73da4f8c13701e004c3bafa939d524
Luna Liu <lunal22@yorkhouse.ca>
2021-01-16 18:07:11 -0800

    changes

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,14 @@ const app = express()
 const port = 3000
 const SERVER_STARTED = 'Server started on port: '
 
+/* app.get('/', (req, res) => {
+    res.send("Hello, World");
+}); */
+
 const db = new Firestore({
     projectId: 'adventure-story-301904',
     keyFilename: 'adventure-story-301904-a988dcece07e.json',
 });
-
-/* app.get('/', (req, res) => {
-    res.send("Hello, World");
-}); */
 
 app.listen(port, () => {
     console.log(SERVER_STARTED + port);
@@ -27,17 +27,21 @@ async function removePage(db){
 } 
 
 async function unConnect (db){
-    const messageRef = db.collection('stories').doc('ts1') .remove('config');
+    const messageRef = db.collection('stories').doc('ts1').remove('config');
 }
 
 async function addPage(db){
     const ref = await db.collection('stories').add({
-        name: '',
-        choice: ''
+        Title: '',
+        Author: '',
+        Parent: '',
+        Zoom: '',
+        Position:{x:float , y:float},
+        Published: true
     }); 
     console.log('Add: ', res);
 } 
 
 async function connectPage(db){
-    const messageRef = db.collection('stories').doc('ts1') .collection('config');
+    const messageRef = db.collection('stories').doc('ts1').collection('config');
 }
